1b89f266230c34a0fd24356c6a996cdeca914c33
EveAnita <evelynanita2001@gmail.com>
2026-03-25 23:52:42 -0300

    Tipos Primitivos

--- Aula02TiposPrimitivos.java
+++ Aula02TiposPrimitivos.java
@@ -1,6 +1,17 @@
 public class Aula02TiposPrimitivos {
     static void main(String[] args){
         int age = 18;
+        long numero = 100000;
+        double salario = 2000;
+        float salarioFloat = 2500;
+        byte ageByte = 18;
+        short ageShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041'; //apenas caracter
+
         System.out.println("A idade é :"+ age);
+        System.out.println(falso);
+        System.out.println("char: "+ caractere);
     }
 }
