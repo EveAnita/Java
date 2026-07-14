0e063c536f441dd4a6a85179c6c142b37a2682bf
EveAnita <evelynanita2001@gmail.com>
2026-07-14 19:09:26 -0300

    .gitignore e adicionando arquivos modificados

--- Aula03TiposPrimitivos.java
+++ Aula03TiposPrimitivos.java
@@ -1,5 +1,8 @@
 public class Aula03TiposPrimitivos {
     static void main (String[] args){
+        int idade = (int) 10000000000L;
+        float salarioFloat = (float) 2500.00;
 
+        System.out.println("Salario da funcionária Evelyn é "+salarioFloat);
     }
 }
---
+++ Aula04OperadoresAritméticos.java
@@ -0,0 +1,8 @@
+public class Aula04OperadoresAritméticos { //Casa
+
+            public static void main(String[] args){ //Cômodo
+                int numero1 = 10;
+                int numero2 = 20;
+                System.out.println((double)numero2 / numero1);
+            }
+}
